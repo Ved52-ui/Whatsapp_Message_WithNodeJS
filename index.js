23ef5e26affd6181c930e78267b08b0b84988cf4
Ved Patel <137433618+Ved52-ui@users.noreply.github.com>
2025-07-30 12:07:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const express = require('express');
-const cors = require('cors');  // <--- I]]o
+const cors = require('cors');  // <--- I]
 
 const app = express();
 app.use(cors());               // <--- Allow frontend acces
